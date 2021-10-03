7743750babd536ffff3220c03edf7683db5aa7d0
Manuel Ortiz Villamarin <m.ortizv2@uniandes.edu.co>
2021-10-03 18:19:27 -0500

    Experimento 2 - Actualizar app en microservicio historia-clinica

--- experimento-2/historia-clinica/flaskr/app.py
+++ experimento-2/historia-clinica/flaskr/app.py
@@ -1,3 +1,18 @@
 from flaskr import create_app
+from flask_restful import Api
+from .vistas import VistaHistoriaClinica
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS, cross_origin
 
 
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+cors = CORS(app)
+
+api = Api(app)
+
+api.add_resource(VistaHistoriaClinica, '/historia_clinica')
+
+jwt = JWTManager(app)
